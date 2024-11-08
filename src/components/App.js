5b6e5d9a23c9fc06d35b5525cee7e22ab941eed4
yujin-1092 <kunche1092@gmail.com>
2024-11-08 12:20:49 +0900

    auth 서비스 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
+import React, { useState } from 'react';
 import './App.css';
+import { authService } from '../firebase';
 import AppRouter from './Router';
+console.log(authService);
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <AppRouter/>
+    <AppRouter isLoggedIn={isLoggedIn}/>
   );
 }
 
