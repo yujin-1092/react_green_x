aeed4b21f8cfbd9e2241e4d7d251d4ebf2e8a409
yujin-1092 <kunche1092@gmail.com>
2024-11-08 11:01:02 +0900

    router 페이지 생성

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/App';
 import { firebase } from './firebase';
+import { BrowserRouter } from 'react-router-dom';
 console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
