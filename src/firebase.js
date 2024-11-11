ea93232c1d5c79e89e6bcefd0d078d3263347aa8
yujin-1092 <kunche1092@gmail.com>
2024-11-11 09:52:47 +0900

    Firestore 초기화

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,5 @@ const app = initializeApp(firebaseConfig);
 
 // export const firebase = initializeApp(firebaseConfig);
 export const authService = getAuth(app);
+
+export const db = getFirestore(app);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,30 +1,28 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 
 const Home = ()=>{
   const [comment, setComment] = useState('');
-
-  const onChange=(e)=>{
+  
+  const onChange = (e)=>{
     // let value = e.target.value;
-    const {target:{value}} = e; //비구조할당
+    const {target:{value}} = e;
     setComment(value);
   }
-
-  const onSubmit= (e)=>{
-    e.preventDefalut();
-
+  const onSubmit = (e)=>{
+    e.preventDefault();
   }
 
   return(
     <div className="container">
-    <Form onSubmit={onsubmit}>
-      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
-      </Form.Group>
-      <Button variant="primary">입력</Button>
-    </Form>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">          
+          <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
+        </Form.Group>
+        <Button variant="primary">입력</Button>
+      </Form>
     </div>
   )
-};
+}
 export default Home;
