bd92b4a97bf4827e2d781cfb86a65b5dcc2b1bb6
yujin-1092 <kunche1092@gmail.com>
2024-11-11 09:46:27 +0900

    onSubmit

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from 'react-bootstrap/Form';
 import { Button } from 'react-bootstrap';
 
 const Home = ()=>{
+  const [comment, setComment] = useState('');
+
+  const onChange=(e)=>{
+    // let value = e.target.value;
+    const {target:{value}} = e; //비구조할당
+    setComment(value);
+  }
+
+  const onSubmit= (e)=>{
+    e.preventDefalut();
+
+  }
+
   return(
     <div className="container">
-    <Form>
+    <Form onSubmit={onsubmit}>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-        <Form.Control type="email" placeholder="글을 입력해주세요" />
+        <Form.Control type="email" onChange={onChange} placeholder="글을 입력해주세요" />
       </Form.Group>
       <Button variant="primary">입력</Button>
     </Form>
