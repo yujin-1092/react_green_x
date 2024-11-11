d92ce0b3970b357378c64356a402748dde1cb574
yujin-1092 <kunche1092@gmail.com>
2024-11-11 11:19:49 +0900

    수정삭제 버튼 추가

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { Link } from 'react-router-dom';
 
 const Comment = ({commentObj})=>{
   return(
-    <ListGroup.Item>{commentObj.comment}</ListGroup.Item> 
+    <ListGroup.Item>
+    <div className='d-flex justify-content-between'>
+      {commentObj.comment}
+        <div className='d-flex gap-1'>
+        <Button variant="secondary" size="sm">수정</Button>
+        <Button variant="danger" size="sm">삭제</Button>
+        </div>
+    </div>
+    </ListGroup.Item> 
   )
 }
 export default Comment;
