54d272455c9b517b42143d2d71c657eaeafeb44e
yujin-1092 <kunche1092@gmail.com>
2024-11-11 09:40:34 +0900

    입력 양식

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,17 @@
 import React from "react";
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
 
 const Home = ()=>{
   return(
-    <div>Home</div>
+    <div className="container">
+    <Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Control type="email" placeholder="글을 입력해주세요" />
+      </Form.Group>
+      <Button variant="primary">입력</Button>
+    </Form>
+    </div>
   )
 };
 export default Home;
